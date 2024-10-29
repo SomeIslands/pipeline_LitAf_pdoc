1946da49363b4e90f85bee94f263d2c847f4a5c5
SomeIslands <altforfills@gmail.com>
2024-10-29 01:23:06 +0000

    Refatoração

--- pages/gráficos.py
+++ pages/gráficos.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
+
 def dados_origem(data):
     st.subheader("Dados de origem")
     st.dataframe(data)
@@ -46,45 +47,6 @@ def status_emprego(data):
     fig = px.pie(job_counts, values='counts', names=job_counts.index)
     st.plotly_chart(fig)
 
-def distribuição_idade(data):
-    # Criar faixas etárias
-    data['age_group'] = pd.cut(data['age'], bins=[0, 18, 30, 45, 60, 100], labels=['0-18', '19-30', '31-45', '46-60', '61+'])
-
-    #distribuição geral por idade
-    age_group_counts = data['age_group'].value_counts().sort_index()
-    #Mostra o histograma geral de idades
-    st.divider()
-    st.subheader("Distribuição por Idade")
-    # Criando o gráfico de barras agrupadas
-    fig_grouped_age = px.bar(age_group_counts,
-                     labels={"value": "Número de Pessoas", "variable": "Idade"}, 
-                     barmode='stack')
-
-    fig_grouped_age.update_layout(xaxis_title="Idades")
-
-    st.plotly_chart(fig_grouped_age)
-
-def distribuição_genero_idade(data):
-    # Criar faixas etárias
-    data['age_group'] = pd.cut(data['age'], bins=[0, 18, 30, 45, 60, 100], labels=['0-18', '19-30', '31-45', '46-60', '61+'])
-
-    #distribuição geral por idade
-    age_group_counts = data['age_group'].value_counts().sort_index()
-
-    #distribuição por gênero e por idade
-    age_gender_distribution = data.groupby(['age_group', 'gender'], observed=True).size().unstack(fill_value=0)
-    #Mostra o histograma geral de idades agrupado por gênero
-    st.divider()
-    st.subheader("Distribuição por Idade e Gênero")
-    # Criando o gráfico de barras agrupadas
-    fig_grouped_gen = px.bar(age_gender_distribution,
-                     labels={"value": "Número de Pessoas", "variable": "Idade"}, 
-                     barmode='group')
-
-    fig_grouped_gen.update_layout( xaxis_title="Idades")
-
-    st.plotly_chart(fig_grouped_gen)
-
 
 #configurando o titulo da janela
 st.set_page_config(page_title="Indicadores referente ao arquivo CSV", page_icon="👁")
@@ -98,10 +60,11 @@ data = pd.read_csv(file)
 #renderizações no Streamlit
 st.title("Indicadores referente ao arquivo CSV")
 
+
+
+
 dados_origem(data)
 gender_agrupamento_data(data)
 exibir_agrupamento_geração(data)
 anos_educação_por_genero(data)
 status_emprego(data)
-distribuição_idade(data)
-distribuição_genero_idade(data)
